e35390ecfaaceefbb76b7900e0d34987dfc91496
axlbasilioo <64106140+axlbasilioo@users.noreply.github.com>
2022-09-26 19:58:19 -0500

    Add files via upload

---
+++ js/writeaboutme.js
@@ -0,0 +1,33 @@
+document.write('<a><h1>About me</h1></a>\
+\
+<p>I\'m a multi plataform software engineer graduated at <a class="uni">University of South West of Guanajuato (UTSOE)</a>. After my Thesis i worked with networking installations, CCTV and developing Apps for Android while i was implementing new Tools to automate process with Cloud access.\
+            </p>\
+        <h2>Some Technologies i\'ve work with</h2>\
+        <div class="myTools">\
+            <div>\
+                <ul>\
+                    <li>JavaScript ES6+</li><br>\
+                    <li>HTML5 and CSS3</li><br>\
+                    <li>Google Cloud Plataform</li>\
+                </ul>\
+            </div>\
+            <div>\
+                <ul>\
+                    <li>Node.Js</li><br>\
+                    <li>React.Js</li><br>\
+                    <li>AWS</li>\
+                </ul>\
+            </div>\
+            <div>\
+                <ul>\
+                    <li>PHP</li><br>\
+                    <li>Android Studio</li><br>\
+                    <li>Azure DB</li>\
+                </ul>\
+            </div>\
+        </div>\
+');
+
+
+
+            
---
+++ js/writeaboutprojects.js
@@ -0,0 +1,24 @@
+document.write('<br>\
+        <h1 class="miencabezado">Some projects i Dev</h1>\
+        <h4 class="misubtitulo">or worked in them</h4>\
+		<div class="myTools">\
+			<div class="imagecontainer" href="google.com">\
+                <a href="https://www.google.com" target="_blank">\
+                    <img class="projectImage" src="https://www.sinrumbofijo.com/wp-content/uploads/2016/05/default-placeholder.png" ><br>\
+                    <a>subtitulo proyecto</a>\
+                </a>\
+			</div>\
+			<div class="imagecontainer">\
+				<a href="https://www.google.com" target="_blank">\
+                    <img class="projectImage" src="https://www.sinrumbofijo.com/wp-content/uploads/2016/05/default-placeholder.png" ><br>\
+                    <a>subtitulo proyecto</a>\
+                </a>\
+			</div>\
+			<div class="imagecontainer">\
+				<a href="https://www.google.com" target="_blank">\
+                    <img class="projectImage" src="https://www.sinrumbofijo.com/wp-content/uploads/2016/05/default-placeholder.png" ><br>\
+                    <a>subtitulo proyecto</a>\
+                </a>\
+			</div>\
+		</div>\
+');
